e43d36f155fa234b265786df73abf8fc23e0ccfc
Pooja <anbhulepooja250@gmail.com>
2020-02-01 22:53:53 +0530

    Initial Commit

---
+++ Employe_Details_/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Employe_Details_
+//
+//  Created by Deep on 01/02/20.
+//  Copyright © 2020 Deep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Employe_Details_Tests/Employe_Details_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Employe_Details_Tests.swift
+//  Employe_Details_Tests
+//
+//  Created by Deep on 01/02/20.
+//  Copyright © 2020 Deep. All rights reserved.
+//
+
+import XCTest
+@testable import Employe_Details_
+
+class Employe_Details_Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
